c2ad7912cd411162054a14d2c955c1efc19aa397
CameronKojo <107430956+CameronKojo@users.noreply.github.com>
2023-09-05 17:48:38 +0300

    add router file

--- index.js
+++ index.js
@@ -1,23 +1,14 @@
 import express from 'express'
 import ("./DB.js")
-import Post from "./post.js"
+import router from "./routers/router.js";
+
 
 const PORT = 5000
 
 const app = express()
 
  app.use(express.json())
-
-app.post('/', async (req, res) => {
-    try {
-        const {author, title, content, picture} = req.body
-        console.log(author, title, content, picture)
-        const post = await Post.create({author, title, content, picture})
-        res.status(200).json(post)
-    } catch (e) {
-        res.status(500).json(e)
-    }
-})
+app.use('/api', router)
 
 //  async function startApp() {
 //     try{
