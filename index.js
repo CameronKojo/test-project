dca65c45a64ab907177864ef98569546d9dbacff
CameronKojo <107430956+CameronKojo@users.noreply.github.com>
2023-09-05 12:26:08 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import Post from './post.js'
+
+
+const PORT = 5000
+const DB_URL = 'mongodb+srv://userIvan:user@cluster0.6bbbifd.mongodb.net/?retryWrites=true&w=majority'
+
+const app = express()
+
+app.use(express.json())
+
+app.post('/', async (req, res) => {
+    const {author, title, content, picture} = req.body
+    const Post = await Post.create({author, title, content, picture})
+    res.status(200).json('Server Working')
+})
+
+async function startApp() {
+    try{
+        await mongoose.connect(DB_URL, {useUnifiedTopology: true, useNewUrlParser: true })
+        app.listen(PORT, () => console.log('SERVER STARTED ON PORT ' + PORT))
+    } catch(e) {
+        console.log(e)
+    }
+}
+
+startApp()
