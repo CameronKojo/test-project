684ba6a89fdab5fb7c0069e93028b92d41cb2641
CameronKojo <107430956+CameronKojo@users.noreply.github.com>
2023-09-06 21:12:35 +0300

    add fileService  add static directory

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 import express from 'express'
 import ("./DB.js")
 import router from "./routers/router.js";
-
+import fileUpload from 'express-fileupload'
 
 const PORT = 5000
-
 const app = express()
 
- app.use(express.json())
+app.use(express.json())
+app.use(express.static('static'))
+app.use(fileUpload({}))
 app.use('/api', router)
 
 //  async function startApp() {
--- postController.js
+++ postController.js
@@ -1,11 +1,9 @@
 import Post from "./post.js";
-
+import PostService from "./PostService.js";
 class PostController {
     async create(req, res){
         try {
-            const {author, title, content, picture} = req.body
-            console.log(author, title, content, picture)
-            const post = await Post.create({author, title, content, picture})
+            const post = await PostService.create(req.body, req.files.picture)
             res.status(200).json(post)
         } catch (e) {
             res.status(500).json(e)
@@ -14,7 +12,7 @@ class PostController {
 
     async getAll(req, res){
         try {
-            const posts = await Post.find();
+            const posts = await PostService.getAll()
             return res.json(posts);
         } catch (e) {
             res.status(500).json(e)
@@ -22,11 +20,7 @@ class PostController {
     }
     async getOne(req, res){
         try {
-            const {id} = req.params
-            if(!id) {
-                res.status(400).json( {message: 'id do not specified'})
-            }
-            const post = await Post.findById(id);
+            const post = await PostService.getOne(req.params.id)
             return res.json(post)
         } catch (e) {
             res.status(500).json(e)
@@ -35,24 +29,16 @@ class PostController {
     }
     async update(req, res){
         try {
-            const post = req.body
-            if(!post._id) {
-                res.status(400).json( {message: 'id do not specified'})
-            }
-            const updatedPost = await Post.findByIdAndUpdate(post._id, post, {new: true})
+            const updatedPost = await PostService.update(req.body);
             return res.json(updatedPost);
         } catch (e) {
-            res.status(500).json(e)
+            res.status(500).json(e.message)
         }
 
     }
     async delete(req, res){
         try {
-            const {id} = req.params
-            if(!id) {
-                res.status(400).json( {message: 'id do not specified'})
-            }
-            const post = await Post.findByIdAndDelete(id);
+            const post = await PostService.delete(req.params.id);
             return res.json(post)
         } catch (e) {
             res.status(500).json(e)
